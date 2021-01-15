997520ff27d81a880b0e6752ca137f6e62156946
2cpt1mp <2cpt1mp@gmail.com>
2021-01-15 22:12:53 +0300

    dispalays list of items

--- routes/items-router.js
+++ routes/items-router.js
@@ -3,46 +3,26 @@ const itemsRouter = Express.Router();
 
 const dbConnection = require('../firebird-controller').Connection;
 
-class MenuItem {
-  constructor(id, title, price) {
-    this.id = id;
-    this.title = title;
-    this.price = price;
-  }
-}
 
-
-function getAllItems() {
-  let menuItems = [];
-
+function getAllItems(callback) {
   dbConnection.query('SELECT * FROM menu_item;', (error, result) => {
     if (error) throw error;
 
-    menuItems = result.fetch("all", true, (row) => {
-
-      //var menuItem = new MenuItem(row.ID, row.TITLE, row.PRICE);
-
-      let obj = {
-        title: 1,
-        id: "wdwd"
-      }
-
-      /*
-      var menuItem = {
+    let menuItems = [];
+    result.fetch("all", true, (row) => {
+      let menuItem = {
         id: row.ID,
         title: row.TITLE,
-        price: row.PRICE
-      }*/
-
-      //console.log(menuItem);
-      //menuItems.push(menuItem);
-      menuItems.push(obj);
-
+        price: row.PRICE,
+        description: 'with id ' + row.ID,
+        picUrl: 'imgs/dish.svg'
+      };
+      menuItems.push(menuItem);
     }, (err, eof) => {
+      if (err) throw err;
 
+      callback(menuItems);
     });
-
-
   });
 }
 
@@ -51,18 +31,10 @@ function getAllItems() {
 itemsRouter.get('/', (req, res) => {
   const DEFAULT_PIC_URL = 'imgs/dish.svg';
 
-  getAllItems();
-  /*
-  let menuItems = [
-    {id: 0, title: 'first', description: '1-st', price: 0.0, picUrl: DEFAULT_PIC_URL },
-    {id: 1, title: 'second', description: '2-nd', price: 0.0, picUrl: DEFAULT_PIC_URL },
-    {id: 2, title: 'third', description: '3-rd', price: 0.0, picUrl: DEFAULT_PIC_URL },
-    {id: 3, title: 'fourth', description: '4-th', price: 0.0, picUrl: DEFAULT_PIC_URL },
-  ];
-*/
+  getAllItems( (items) => {
+    res.render('items', {menuItems: items})
+  });
 
-
-  //res.render('items', {menuItems: menuItems});
 })
 
 module.exports = itemsRouter;
