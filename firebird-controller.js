b71c0b9cb474163c82f6fe3df2e8609d1b5ae7fe
Blade Wolfmoon <2cpt1mp@gmail.com>
2021-02-03 21:20:25 +0300

    adapted for linux based machine

--- firebird-controller.js
+++ firebird-controller.js
@@ -1,7 +1,7 @@
 const Firebird = require('firebird');
 const dbConnection = Firebird.createConnection();
 
-dbConnection.connect('192.168.1.3:C:\\SHOP.FDB', 'SYSDBA', 'masterkey', '', (err) => {
+dbConnection.connect('localhost:/home/blade/DB/SHOP.FDB', 'SYSDBA', 'masterkey', '', (err) => {
   if (err)
     throw err;
 });
